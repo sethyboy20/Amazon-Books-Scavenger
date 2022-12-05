238a97543a4300e299a3690558091344615e7c3b
SufiaRashid <119264407+SufiaRashid@users.noreply.github.com>
2022-12-05 18:03:47 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,140 @@
+import java.util.*;
+
+public class Book {
+    private String bookID;
+    private String title;
+    private String desc;
+    private double price;
+    private ArrayList<String> authors = new ArrayList<>();
+    private String image;
+    private String previewLink;
+    private String publisher;
+    private String publishedDate;
+    private String infoLink;
+    private ArrayList<String> categories = new ArrayList<>();
+    private double ratingsCount;
+
+    public Book() {
+        this.bookID = "";
+        this.title = "";
+        this.desc = "";
+        this.price = 0;
+        this.image = "";
+        this.previewLink = "";
+        this.publisher = "";
+        this.publishedDate = "";
+        this.infoLink = "";
+        this.ratingsCount = 0;
+    }
+
+    public void setBookID(String bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookID() {
+        return bookID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void addAuthor(String authorName) {
+        authors.add(authorName);
+    }
+
+    public ArrayList<String> getAuthors() {
+        return authors;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setPrevLink(String previewLink) {
+        this.previewLink = previewLink;
+    }
+
+    public String getPrevLink() {
+        return previewLink;
+    }
+
+    public void setPub(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPub() {
+        return publisher;
+    }
+
+    public void setPubDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getPubDate() {
+        return publishedDate;
+    }
+
+    public void setInfoLink(String infoLink) {
+        this.infoLink = infoLink;
+    }
+
+    public String getInfoLink() {
+        return infoLink;
+    }
+
+    public void addCategory(String catName) {
+        categories.add(catName);
+    }
+
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+    public void setRatingsCount(double ratingsCount) {
+        this.ratingsCount = ratingsCount;
+    }
+
+    public double getRatingsCount() {
+        return ratingsCount;
+    }
+
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                image + '\'' +
+                "description:" + desc + '\'' +
+                ", authors='" + authors + '\'' +
+                ", previewLink='" + previewLink + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", publishedDate='" + publishedDate + '\'' +
+                ", infoLink='" + infoLink + '\'' +
+                ", categories='" + categories + '\'' +
+                ", ratingCount='" + ratingsCount + '\'' +
+                '}';
+    }
+}

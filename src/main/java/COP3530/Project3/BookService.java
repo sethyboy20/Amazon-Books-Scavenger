eaaaf59ea24b8bb0c5f8bc113471d5e014cad3af
Seth Paul <seth.paul@ufl.edu>
2022-12-05 07:28:13 -0500

    additional search options + hashmap integration

--- src/main/java/COP3530/Project3/BookService.java
+++ src/main/java/COP3530/Project3/BookService.java
@@ -7,17 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 @Service
 public class BookService {
     private ArrayList<Book> book;
+    Hash hashBooks = new Hash();
 
     @Autowired
-    BookService(ArrayList<Book> book){
-        this.book = book;
+    BookService(ArrayList<Book> book) throws Exception{
+        hashBooks.read();
+        /*this.book = book;
         for(int i = 1;i<10_026;i++) {
             addBook(this.book, "Test"+i);
-        }
+        }*/
     }
 
     private void addBook(ArrayList<Book> book, String test) {
@@ -38,6 +41,24 @@ public class BookService {
     }
 
     public List<Book> findBooksByTitle(String search) {
-        return book.stream().filter(c->c.getTitle().toLowerCase(Locale.US).contains(search.toLowerCase(Locale.US))).collect(Collectors.toList());
+        //return book.stream().filter(c->c.getTitle().toLowerCase(Locale.US).contains(search.toLowerCase(Locale.US))).collect(Collectors.toList());
+        return hashBooks.books.values().stream().filter(c->c.getTitle().toLowerCase(Locale.US).contains(search.toLowerCase(Locale.US))).collect(Collectors.toList());
+    }
+    
+    public List<Book> findBooksByDesc(String search) {
+        return hashBooks.books.values().stream().filter(c->c.getDesc().toLowerCase(Locale.US).contains(search.toLowerCase(Locale.US))).collect(Collectors.toList());
+    }
+    
+    public List<Book> findBooksByAuthor(String search) {
+        return hashBooks.books.values().stream().filter(c->authorSearchHelper(c, search)).collect(Collectors.toList());
+    }
+    
+    private boolean authorSearchHelper(Book book, String search) {
+        for (int i = 0; i < book.getAuthors().size(); i++) {
+            if (book.getAuthors().get(i).toLowerCase(Locale.US).contains(search.toLowerCase(Locale.US)))
+                return true;
+        }
+        
+        return false;
     }
 }
